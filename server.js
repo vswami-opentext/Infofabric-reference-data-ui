927da68bc6de93d614ee2868cc19c5b775a0aa2b
Vishnu Swami <vswami@opentext.com>
2020-12-28 14:49:35 +0530

    version change

--- server.js
+++ server.js
@@ -3,16 +3,13 @@ const express = require('express')
 const app = express()
 const axios = require('axios');
 const bodyParser = require('body-parser')
-// const session = require('express-session') // -> session storage using mongo
 const path = require('path')
 
-// app.use(session({ secret: 'zz', resave: true, saveUninitialized: true }))
 app.use(bodyParser.json())
 
 app.use(express.static(path.join(__dirname, './dist/infofabric-reference-data-ui'))) // BUT ON PRODUCTION -> nginx
 
 app.all('/api/*', async(req, res) => {
-	// req.session.quote = req.body.quote;
 	try{
 		return axios.get(process.env.API_URL+req.url)
 			   .then(data => res.status(200).send(data.data))
