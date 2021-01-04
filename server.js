6aec2182f5ec043cb011b80d89ddabce547531bd
Vishnu Swami <vswami@opentext.com>
2021-01-04 11:36:53 +0530

    relative path

--- server.js
+++ server.js
@@ -7,21 +7,27 @@ const path = require('path')
 
 app.use(bodyParser.json())
 
-app.use(express.static(path.join(__dirname, './dist/infofabric-reference-data-ui'))) // BUT ON PRODUCTION -> nginx
+app.use(express.static(path.join(__dirname, './dist/infofabric-reference-data-ui')))
 
 app.all('/api/*', async(req, res) => {
 	try{
+		console.log('url--->', process.env.API_URL+''+req.url)
 		return axios.get(process.env.API_URL+req.url)
 			   .then(data => res.status(200).send(data.data))
 			   .catch(err => res.send(err));
 	 }
 	 catch(err){
-		console.error("GG", err);
+		console.error("api call error on all paths:", err);
 	 }
 })
-app.get('/health', (req, res) => {
+
+app.get('/reference-data-ui/health', (req, res) => {
+	console.log('Health checked');
     res.json({status: 'UP'});
 })
+const _app_folder = __dirname + '/dist/infofabric-reference-data-ui';
+
+app.use('/reference-data-ui', express.static(_app_folder, {maxAge: '1m'}));
 
 app.get('/*', (req, res) => {
 	return res.sendFile(path.join(__dirname, './dist/infofabric-reference-data-ui/index.html'))
